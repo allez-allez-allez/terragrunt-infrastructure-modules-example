371df254b3b317d75f59691724ecc9685bc8be70
Triet Le <triet.le@plus3it.com>
2019-05-08 13:57:50 -0400

    update azs

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -28,7 +28,7 @@ module "vpc" {
   name = "${var.project_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs = ["${data.aws_availability_zones.all.names}"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
@@ -73,7 +73,7 @@ module "vpc" {
 
 resource "aws_autoscaling_group" "webserver_example" {
   launch_configuration = "${aws_launch_configuration.webserver_example.id}"
-  availability_zones   = ["${module.vpc.azs}"]
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
   load_balancers    = ["${aws_elb.webserver_example.name}"]
   health_check_type = "ELB"
